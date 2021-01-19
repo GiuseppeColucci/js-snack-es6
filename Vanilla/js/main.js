997c6108b7175eb9892d0740d9dfaf656b9c1755
giuseppecolucci <giuseppecolucciwork@gmail.com>
2021-01-19 17:20:49 +0100

    finito anche es5/vanilla

--- Vanilla/js/main.js
+++ Vanilla/js/main.js
@@ -18,3 +18,41 @@ for(var x=1; x<bici.length; x++){
     }
 }
 console.log(biciPeso)
+/**
+ * /**
+ * Creare un array di oggetti di squadre di calcio.
+Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti
+Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+ */
+function Nrandom(max, min){
+    return Math.floor(Math.random() * (max - min)+1) - min;
+}
+ var sq1=[
+     {
+        nome:'Roma',
+        puntiFatti:0,
+        falliSubiti:0
+    },
+    {
+        nome:'inter',
+        puntiFatti:0,
+        falliSubiti:0,
+    },{
+        nome:'milan',
+        puntiFatti:0,
+        falliSubiti:0
+    },
+    
+
+ ];
+
+ for(var y=0; y<sq1.length; y++){
+    var teams=sq1[y];
+    teams. puntiFatti=Nrandom(1, 10);
+    teams.falliSubiti=Nrandom(1, 10);
+    console.log(teams)
+}
+ 
+
